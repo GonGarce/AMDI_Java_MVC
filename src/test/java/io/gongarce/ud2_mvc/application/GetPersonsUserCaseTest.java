be73938dd3c65c0f6e34401636764fb5ad6a248f
GonGarce <gongarce@gmail.com>
2025-01-29 18:48:28 +0100

    Test example for get persons use case

---
+++ src/test/java/io/gongarce/ud2_mvc/application/GetPersonsUserCaseTest.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package io.gongarce.ud2_mvc.application;
+
+import io.gongarce.ud2_mvc.domain.person.Person;
+import io.gongarce.ud2_mvc.domain.person.PersonRepository;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+/**
+ *
+ * @author gag
+ */
+public class GetPersonsUserCaseTest {
+
+    PersonRepository personRepository;
+
+    private final GetPersonsUserCase personUseCase;
+
+    public GetPersonsUserCaseTest() {
+        personRepository = Mockito.mock(PersonRepository.class);
+        this.personUseCase = new GetPersonsUserCase(personRepository);
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void shouldReturnAllPersonsWhenEmptyOptional() {
+        // with
+        Optional<String> empty = Optional.empty();
+        List<Person> expected = Arrays.asList(
+                new Person(1l, "123456789A", "Gonzalo", "A Coruña", null, null),
+                new Person(2l, "123456789A", "Alejandro", "Madrid", null, null));
+        Mockito.when(personRepository.getAll()).thenReturn(expected);
+
+        // when
+        List<Person> actual = personUseCase.get(empty);
+
+        // then
+        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected.get(0), actual.get(0));
+        Mockito.verify(personRepository, Mockito.atMostOnce()).getAll();
+        Mockito.verify(personRepository, Mockito.never()).getByMail(Mockito.any());
+    }
+
+    @Test
+    public void shouldReturnPersonsWithEmailWhenPresent() {
+        // with
+        String email = "gonzalo@gmail.com";
+        List<Person> expected = Arrays.asList(
+                new Person(1l, "123456789A", "Gonzalo", "A Coruña", null, null),
+                new Person(2l, "123456789A", "Alejandro", "Madrid", null, null));
+        Mockito.when(personRepository.getByMail(email)).thenReturn(expected);
+
+        // when
+        List<Person> actual = personUseCase.get(Optional.of(email));
+
+        // then
+        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected.get(0), actual.get(0));
+        Mockito.verify(personRepository, Mockito.atMostOnce()).getByMail(email);
+        Mockito.verify(personRepository, Mockito.never()).getAll();
+    }
+
+}
